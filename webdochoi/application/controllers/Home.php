68d5ca084b8d2440186fd9e2d011965bd2874417
ngoclt_58 <ngoclt_58@vnu.edu.vn>
2016-06-10 16:34:03 +0700

    ngoclt up
    them chuc nang slide, news, danh muc san pham va quan li nguoi dung
    trong giao dien admin

--- webdochoi/application/controllers/Home.php
+++ webdochoi/application/controllers/Home.php
@@ -6,10 +6,17 @@ Class Home extends CI_Controller{
         $this->load->model('product_model');
     }
     
-    function index(){
+    function index()
+    {
         $input = array();
-        $input['order'] = array('created','DESC');
-        $input['limit'] = array('3' ,'0');
+        $input['order'] = array('created', 'DESC');
+        $nums_record = 3;
+        if(isset($_GET['page'])) {
+            $input['limit'] = array($nums_record * $_GET['page'], $nums_record * $_GET['page'] - $nums_record);
+        }
+        else {
+            $input['limit'] = array($nums_record, "0");
+        }
         $list = $this->product_model->get_list($input);
         $data['total'] = $this->product_model->get_total();
         $data['list'] = $this->product_model->get_list($input);
